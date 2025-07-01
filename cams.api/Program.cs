fde3695e0e64bff804ecf990cb7ef122c9246b9f
Renato Martins <guillenmartins@gmail.com>
2025-07-01 16:31:13 +0100

    Enhance documentation: add Swagger configuration for API with metadata and licensing information

--- cams.api/Program.cs
+++ cams.api/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using cams.application;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +20,24 @@ builder.Services.AddSwaggerGen(options =>
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
     options.IncludeXmlComments(xmlPath);
+    
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CAMS - Car Auction Management System",
+        Version = "v1",
+        Description = "Car auction management system API built with ASP.NET Core.",
+        Contact = new OpenApiContact
+        {
+            Name = "Renato Martins",
+            Email = "guillenmartins@gmail.com",
+            Url = new Uri("https://github.com/guimar86/cams")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
 });
 builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
